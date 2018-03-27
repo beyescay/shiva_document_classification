d846c13365edda14c649d093d53395ce62d5521b
beyescay <beyescay@gmail.com>
2018-03-26 21:49:22 -0500

    Modified the intro script according to the model needs.

--- clean_data.py
+++ clean_data.py
@@ -7,21 +7,23 @@ class DataCleaning:
     def __init__(self, path_to_20_newsgroups_dir):
         self.path_to_20_newsgroups_dir = path_to_20_newsgroups_dir
         data_dir = self.create_training_and_testing_data_set()
-        self.clean_data(data_dir)
-        self.merge_data(data_dir)
+        self.remove_header_lines(data_dir)
+        self.merge_and_clean_data(data_dir)
 
     def create_training_and_testing_data_set(self):
         os.makedirs(os.path.join(os.getcwd(), "data", "training"))
         os.makedirs(os.path.join(os.getcwd(), "data", "testing"))
-        os.makedirs(os.path.join(os.getcwd(), "data", "unwanted"))
+
+        key_words = ["comp", "politics", "rec", "sport"]
+
+        for key_word in key_words:
+            os.mkdir(os.path.join(os.getcwd(), "data", "training", key_word))
+            os.mkdir(os.path.join(os.getcwd(), "data", "testing", key_word))
 
         test_data_folder = os.path.join(os.path.join(os.getcwd(), "data", "testing"))
         train_data_folder = os.path.join(os.path.join(os.getcwd(), "data", "training"))
-        unwanted_data_folder = os.path.join(os.path.join(os.getcwd(), "data", "unwanted"))
 
         testing_directories = ["comp.windows.x", "rec.sport.baseball", "talk.politics.misc", "rec.autos"]
-
-        training_key_words = ["comp", "politics", "rec"]
 
         data_directories = glob.glob(os.path.join(self.path_to_20_newsgroups_dir, "*"))
 
@@ -31,26 +33,23 @@ class DataCleaning:
             news_group_folder_name = os.path.basename(data_dir)
 
             if news_group_folder_name in testing_directories:
-                print("Goes to testing")
-                copy_tree(data_dir, os.path.join(test_data_folder, news_group_folder_name))
+
+                for test_key_word in news_group_folder_name.split('.'):
+                    if test_key_word in key_words:
+                        if test_key_word == 'rec' and len(news_group_folder_name.split('.')) == 3:
+                            continue
+                        copy_tree(data_dir, os.path.join(test_data_folder, test_key_word, news_group_folder_name))
 
             else:
-                is_training = False
-                for key_word in news_group_folder_name.split('.'):
-                    if key_word in training_key_words:
-                        print("Goes to training")
-                        copy_tree(data_dir, os.path.join(train_data_folder, news_group_folder_name))
-                        is_training = True
-                        break
-
-                if not is_training:
-                    print("Goes to unwanted")
-                    print(os.path.join(unwanted_data_folder, news_group_folder_name))
-                    copy_tree(data_dir, os.path.join(unwanted_data_folder, news_group_folder_name))
+                for train_key_word in news_group_folder_name.split('.'):
+                    if train_key_word in key_words:
+                        if train_key_word == 'rec' and len(news_group_folder_name.split('.')) == 3:
+                            continue
+                        copy_tree(data_dir, os.path.join(train_data_folder, train_key_word, news_group_folder_name))
 
         return os.path.join(os.getcwd(), "data")
 
-    def clean_data(self, data_directory):
+    def remove_header_lines(self, data_directory):
         for root, dir, files in os.walk(data_directory):
             if len(files) > 0:
                 for file_basename in files:
@@ -71,10 +70,11 @@ class DataCleaning:
 
                     os.remove(file_name)
 
-    def merge_data(self, data_directory):
+    def merge_and_clean_data(self, data_directory):
         for root, dir, files in os.walk(data_directory):
             if len(files) > 0:
-                merged_file_name_to_write = os.path.join(root, "merged.txt")
+                merged_file_name_to_write = os.path.join(root, "{}.txt".format(os.path.basename(root)))
+
                 for file_basename in files:
                     file_name = os.path.join(root, file_basename)
 
@@ -82,10 +82,30 @@ class DataCleaning:
                         lines = file_reader.readlines()
 
                     with open(merged_file_name_to_write, 'a') as file_writer:
-
                         for line in lines:
                             file_writer.write("{}\n".format(line.strip()))
 
+                SH.copy(merged_file_name_to_write, os.path.join(root, "..", "{}.txt".format(os.path.basename(root))))
+
+        for root, dirs, files in os.walk(data_directory):
+            if len(files) > 0:
+                for dir in dirs:
+                    print dir
+                    SH.rmtree(os.path.join(root, dir))
+
+                merged_file_name_to_write = os.path.join(root, "{}.txt".format(os.path.basename(root)))
+
+                for file_basename in files:
+                    file_name = os.path.join(root, file_basename)
+                    with open(file_name, 'r') as file_reader:
+                        lines = file_reader.readlines()
+
+                    with open(merged_file_name_to_write, 'a') as file_writer:
+                        for line in lines:
+                            file_writer.write("{}\n".format(line.strip()))
+
+                    os.remove(file_name)
+
 
 if __name__ == "__main__":
-    DataCleaning("/home/sarath/Documents/Shiva/032618/20_newsgroups")
+    DataCleaning("/home/sarath/Documents/Shiva/20_newsgroups")

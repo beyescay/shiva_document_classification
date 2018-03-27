d5aa3a40b80bf325702b31ff85218ac1e1ce56ec
beyescay <beyescay@gmail.com>
2018-03-27 01:12:37 -0500

    Working model. Currently, 0.437

--- model_20newsgroup.py
+++ model_20newsgroup.py
@@ -16,6 +16,16 @@ from sklearn import svm
 import string
 import os
 from io import open
+from sklearn.linear_model import RidgeClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.naive_bayes import BernoulliNB, MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import NearestCentroid
+from sklearn.ensemble import RandomForestClassifier
 
 def read_data(fname, labl = "none", flag = 'Train'):
     """
@@ -28,21 +38,21 @@ def read_data(fname, labl = "none", flag = 'Train'):
 
     if flag == 'Train':
 
-        for w in fin.read().lower().split():
-            if w not in stopword_list:
+        for w in fin.read().lower().strip().split():
+            if w not in stopword_list and w != "":
                 words.append(w.lower())    
                 labels.append(str(labl))
         fin.close()
         return words,labels
     else:
-
-        for w in fin.read().lower().split():
-            words.append(w.lower().strip(string.punctuation))  
+        for w in fin.read().lower().strip().split():
+            if w not in stopword_list and w != "":
+                words.append(w.lower().strip(string.punctuation))
         fin.close()
         return words
 
 
-def vectorize_train_data(data_list, train_test_flag, mindf=4, maxdf=0.8):
+def vectorize_train_data(data_list, train_test_flag, mindf=0.01, maxdf=0.7):
     """
     This method reads the list of reviews then converts it to a csr_matrix using sklearns built-in function
     we also remove the stopwords from every review using nltk's list of stopwords
@@ -51,7 +61,7 @@ def vectorize_train_data(data_list, train_test_flag, mindf=4, maxdf=0.8):
     :return: csr_matrix containing tf-idf values of the reviews
     """
     stopword_list = stopwords.words('english')
-    vectorizer = TfidfVectorizer(ngram_range=(1, 2), min_df=mindf, max_df=maxdf, sublinear_tf=True, use_idf=True,
+    vectorizer = TfidfVectorizer(max_df=maxdf, sublinear_tf=True,
                                  stop_words=stopword_list)
 
     data_vector = vectorizer.fit_transform(data_list)
@@ -71,7 +81,7 @@ def vectorize_test_data(test_data_list, vectorizer):
     return data_vector
 
 
-def classify(test_vector, train_vector, train_labels, strategy='linear'):
+def fit_model(train_vector, train_labels, strategy='linear'):
     """
     This method takes in the train and test vectors containing tf-idf scores , train_labels and performs classification
     using SVM(Support Vector Machine, and uses a linear kernel for the strategy) and returns a list of predicted labels
@@ -84,8 +94,12 @@ def classify(test_vector, train_vector, train_labels, strategy='linear'):
 
     :return:  A list of predicted labels for the test data
     """
-    classifier_linear = svm.SVC(kernel=strategy, C=0.8)
+    classifier_linear = RidgeClassifier(tol=1e-2, solver="lsqr")
     classifier_linear.fit(train_vector, train_labels)
+
+    return classifier_linear
+
+def classify(test_vector, classifier_linear):
     prediction_svm_linear = classifier_linear.predict(test_vector)
     return prediction_svm_linear
 
@@ -100,7 +114,7 @@ test_label_cumm_list = []
 
 
 for idx, i in enumerate(['comp', 'sport', 'politics', 'rec']):
-    labelIndPair[count] = i
+    labelIndPair[idx] = i
     train_data, train_label = read_data(os.path.join("data/training/", i, "{}.txt".format(i)),
                                         labl=idx)
 
@@ -112,6 +126,11 @@ for idx, i in enumerate(['comp', 'sport', 'politics', 'rec']):
 
 train_vector, vectorizer = vectorize_train_data(data_list=train_data_cumm_list, train_test_flag="train")
 
+print("Size of train vector:{}".format(train_vector.shape))
+
+print("Training...")
+svm_model = fit_model(train_vector=train_vector, train_labels=train_label_cumm_list, strategy="linear")
+print("Finished training")
 predicted_group_label = []
 actual_group_label = []
 
@@ -122,13 +141,27 @@ for idx, i in enumerate(['comp', 'sport', 'politics', 'rec']):
     for root, dirs, files in os.walk(ith_test_data_dir):
         if len(files) > 0:
             for test_file in files:
+
                 test_file_path = os.path.join(root, test_file)
+                print("Predicting for {}".format(test_file))
                 test_data = read_data(test_file_path, flag='test')
+                if len(test_data) == 0:
+                    print("Empty file. So skipping")
+                    continue
                 test_vector = vectorize_test_data(test_data_list=test_data, vectorizer=vectorizer)
-                predicted_labels = classify(test_vector=test_vector, train_vector=train_vector, train_labels=train_label, strategy="linear")
-                predicted_group = max(predicted_labels)
-                predicted_group_label.append(predicted_group)
+                predicted_labels = classify(test_vector=test_vector, classifier_linear=svm_model)
+                current_test_data_label_count = {}
+                for predicted_label in predicted_labels:
+                    if predicted_label in current_test_data_label_count:
+                        current_test_data_label_count[predicted_label] += 1
+                    else:
+                        current_test_data_label_count[predicted_label] = 1
+                predicted_group = max(current_test_data_label_count, key=current_test_data_label_count.get)
+                predicted_group_label.append(int(predicted_group))
+                #print(current_test_data_label_count)
+                #print("Predicted label: {}".format(labelIndPair[int(predicted_group)]))
+                #print("Actual label: {}".format(labelIndPair[idx]))
                 actual_group_label.append(idx)
 
 
-print("Accuracy: {}".format(sum(predicted_group_label == actual_group_label)/len(predicted_group_label)))
+print("Accuracy: {}".format(sum(1.0 for x, y in zip(predicted_group_label, actual_group_label) if x == y) / float(len(predicted_group_label))))
